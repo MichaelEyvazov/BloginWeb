8fe8fe074be9379eb6002da2222403d23fa57cf6
MichaelEyvazov <michaeleyvazov@gmail.com>
2019-02-10 18:11:24 +0200

    Added modal and dropdow

--- js/main.js
+++ js/main.js
@@ -5,8 +5,23 @@ function init() {
 function toggleMenu() {
     document.body.classList.toggle('open');
     document.querySelector('ul').classList.toggle('flex');
-    // checkMediaSize();
+    var elMenuButton = document.querySelector('.mobile-menu-button');
+    elMenuButton.textContent === '☰' ? elMenuButton.innerHTML = '&times;' : elMenuButton.innerHTML = '☰';
 }
+
+function toggleModal() {
+    document.querySelector('.modal').classList.toggle('show');
+}
+
+function closeModal() {
+    document.querySelector('.modal').classList.remove('show');
+}
+
+// function dropdownToggle() {
+//     var elDropDown = document.querySelector('.dropdown-content');
+//     elDropDown.style.display = ((elDropDown.style.display === 'block') ? 'none' : 'block');
+//     // document.querySelector('.dropdown-content').style.display('flex');
+// }
 
 // function checkMediaSize() {
 //     var x = window.matchMedia('(max-width:1024px)');
